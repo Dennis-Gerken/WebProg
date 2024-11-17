2067deece80129b1d054ee41cbed7b2451ab0740
herzogel121 <eliasher@outlook.de>
2024-11-17 12:21:53 +0100

    Korrekturlesen

--- pixelpiraten/js/index.js
+++ pixelpiraten/js/index.js
@@ -1,12 +1,12 @@
 function saveLoginDataAndRedirect(event) {
-  event.preventDefault(); // Verhindert Standardformular-Aktion
+  event.preventDefault();
   const email = document.getElementById('email').value;
   sessionStorage.setItem('email', email);
   window.location.href = 'benutzer.html';
 }
 
 
-// Event-Listener für das Login-Formular
+
 document.addEventListener("DOMContentLoaded", function () {
   const loginForm = document.getElementById("loginForm");
   if (loginForm) {
@@ -20,12 +20,12 @@ document.addEventListener("DOMContentLoaded", function () {
   const profileSection = document.getElementById("profile-section");
 
   if (email) {
-    // Wenn ein Benutzer angemeldet ist, zeige das Profil-Icon
+
     loginBtn.style.display = "none";
     registerBtn.style.display = "none";
     profileSection.style.display = "block";
   } else {
-    // Wenn niemand angemeldet ist, zeige Anmelden/Registrieren-Buttons
+
     loginBtn.style.display = "block";
     registerBtn.style.display = "block";
     profileSection.style.display = "none";
@@ -39,12 +39,12 @@ counters.forEach(counter => {
     const target = +counter.getAttribute('data-target');
     const current = +counter.innerText;
 
-    // Geschwindigkeit des Zählens
+
     const increment = target / 100;
 
     if (current < target) {
       counter.innerText = Math.ceil(current + increment);
-      setTimeout(updateCounter, 20); // Geschwindigkeit der Animation in Millisekunden
+      setTimeout(updateCounter, 20);
     } else {
       counter.innerText = target;
     }
@@ -60,17 +60,17 @@ document.addEventListener("DOMContentLoaded", function () {
     (entries) => {
       entries.forEach((entry) => {
         if (entry.isIntersecting) {
-          entry.target.classList.add("show"); // Fügt die "show"-Klasse hinzu, wenn sichtbar
+          entry.target.classList.add("show");
         }
       });
     },
     {
-      threshold: 0.1, // Aktiviert die Animation, wenn 10% der Box sichtbar sind
+      threshold: 0.1,
     }
   );
 
   boxes.forEach((box) => {
-    observer.observe(box); // Beobachtet jedes Element mit der Klasse "box"
+    observer.observe(box);
   });
 });
 
@@ -79,11 +79,11 @@ function closePopup() {
 }
 
 document.addEventListener("DOMContentLoaded", function () {
-  // Anmelde- und Registrierungs-Buttons
+
   const anmeldenButton = document.querySelector(".navbar-right button:nth-child(1)");
   const registrierenButton = document.querySelector(".navbar-right button:nth-child(2)");
 
-  // Weiterleiten zu den neuen Seiten
+
   if(anmeldenButton) {
     anmeldenButton.addEventListener("click", function () {
       window.location.href = "../pages/anmelden.html";
@@ -94,7 +94,7 @@ if(registrierenButton) {
     window.location.href = "../pages/registrieren.html";
   });
 }
-  // "MEHR"-Buttons für Gründung, Teams, Mitglieder und Sponsoren
+
   document.getElementById("mehrGruendung").addEventListener("click", function () {
     window.location.href = "../pages/überUns.html";
   });
